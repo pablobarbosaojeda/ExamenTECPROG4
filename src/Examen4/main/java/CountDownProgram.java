66dec7cb5a2b048b9091e618aab4b8a472b17852
Barbosa <pbarboje@myuax.com>
2024-04-29 14:45:58 +0200

    Implementacion del programa cuenta atras agregando un sleep mediante la utilizacion de Thread.sleep  para realizar la espera de 1 s entre cada interración. Se realiza posteriormente la captura de dicha excepción. (CountDownProgram.java)

--- src/Examen4/main/java/CountDownProgram.java
+++ src/Examen4/main/java/CountDownProgram.java
@@ -1,2 +1,19 @@
+
 public class CountDownProgram {
+    public static void main(String[] args) {
+      int num = 10;
+      while (num != 0) {
+        System.out.println(num);
+        num--;
+        sleep(1000); //Esperar 1 segundo entre cada interración
+      }
+        System.out.println("El programa se detiene");
+    }
+    private static void sleep(long milliseconds){
+        try {
+            Thread.sleep(milliseconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
