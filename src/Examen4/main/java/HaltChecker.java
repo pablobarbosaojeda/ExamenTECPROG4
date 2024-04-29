e9ef38b538d8f85197d0ee1f9761fd1801a982ea
Barbosa <pbarboje@myuax.com>
2024-04-29 15:16:38 +0200

    Creamos el buble de comprobación de entrada.

--- src/Examen4/main/java/HaltChecker.java
+++ src/Examen4/main/java/HaltChecker.java
@@ -1,4 +1,10 @@
 public class HaltChecker {
     public static boolean checkHalt(int[] code, int input){
+        for (int instruction : code) {
+            if (instruction == input) {
+                return true; //El programa se detiene si encuentra la entrada específica
+            }
+        }
+        return false; //El programa no se detiene si no encuentra la entrada específica
     }
 }
