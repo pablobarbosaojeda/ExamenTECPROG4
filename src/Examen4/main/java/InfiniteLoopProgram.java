cd377b021468551644ed1f826d6a86875b2168bf
Barbosa <pbarboje@myuax.com>
2024-04-29 14:48:55 +0200

    Creación de la base del bucle de InfiniteLoopProgram.java

--- src/Examen4/main/java/InfiniteLoopProgram.java
+++ src/Examen4/main/java/InfiniteLoopProgram.java
@@ -1,2 +1,9 @@
 public class InfiniteLoopProgram {
+    public static void main(String[] Args){
+        int num = 1;
+        while(true) {
+            System.out.println(num);
+            num++;
+        }
+    }
 }
