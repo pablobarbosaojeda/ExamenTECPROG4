e3f2dfad231006b28d4b5bed1de73ebead244eed
Barbosa <pbarboje@myuax.com>
2024-05-03 10:41:20 +0100

    Implementación del funcionamiento de Reverser.java

--- src/Examen4/main/java/Reverser.java
+++ src/Examen4/main/java/Reverser.java
@@ -1,2 +1,16 @@
 public class Reverser {
+    public static void main(String[] args) {
+        //HaltChecker analiza el código de Reverser
+
+        int[] code = {1, 2, 3, 4, 5}; //Código de Reverser
+        int input = 5; //Entrada específica para Reverser
+
+        //Si HaltChecker indica que el programa se detiene, Reverser ejecuta un bucle infinito
+        if (HaltChecker.checkHalt(code, input)) {
+            while (true) {
+                //Bucle infinito
+            }
+        }
+
+    }
 }
