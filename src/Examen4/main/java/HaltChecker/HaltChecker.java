f4cdfef1c02448688c9acc53a465b44f3c12f98b
Barbosa <pbarboje@myuax.com>
2024-05-09 14:57:08 +0200

    Definición de HaltChecker.java

--- src/Examen4/main/java/HaltChecker/HaltChecker.java
+++ src/Examen4/main/java/HaltChecker/HaltChecker.java
@@ -1,4 +1,14 @@
 package HaltChecker;
 
 public class HaltChecker {
+public boolean checkIfHalt(Program program, int input){
+    if(program.getCode() == 1){
+        return input >= 0; //El contador hacia abajo se detiene si el contador alcanza cero o un número negativo
+    } else if(program.getCode() == 2){
+        return false; //El contador infinito nunca se detiene
+    } else {
+        return input == 0; // Otros programas se detienen solo si la entrada es cero
+
+        }
     }
+}
