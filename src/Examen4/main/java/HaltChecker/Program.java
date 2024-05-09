96acf162f0163e833e65f5e444f24a13aa0bd13b
Barbosa <pbarboje@myuax.com>
2024-05-09 14:50:06 +0200

    Definición de Program.java

--- src/Examen4/main/java/HaltChecker/Program.java
+++ src/Examen4/main/java/HaltChecker/Program.java
@@ -1,4 +1,13 @@
 package HaltChecker;
 
 public class Program {
+    public int code;
+
+    public Program(int code){
+        this.code = code;
+    }
+    public int getCode(){
+        return code;
+    }
 }
+
