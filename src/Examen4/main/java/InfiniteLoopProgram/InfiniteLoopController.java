e412c6d3edeb18e470da7b66e17690ae4e4cec84
Barbosa <pbarboje@myuax.com>
2024-05-09 14:32:59 +0200

    Definicion de InfiniteLoopController.java y constructor

--- src/Examen4/main/java/InfiniteLoopProgram/InfiniteLoopController.java
+++ src/Examen4/main/java/InfiniteLoopProgram/InfiniteLoopController.java
@@ -1,4 +1,12 @@
 package InfiniteLoopProgram;
 
 public class InfiniteLoopController {
+    private InfiniteLoopModel model;
+    private InfiniteLoopView view;
+
+    public InfiniteLoopController(InfiniteLoopModel model, InfiniteLoopView view){
+        this.model = model;
+        this.view = view;
+        model.addObserver(view); //Registrar la vista como observadora del modelo
+    }
 }
